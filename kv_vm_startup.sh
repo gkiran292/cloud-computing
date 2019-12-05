fa42f3140e658b9ae720b0a8f8179ea0c285540f
Gopi Kiran <gkiran@iu.edu>
2019-12-05 01:41:41 -0500

    changing startup script

--- kv_vm_startup.sh
+++ kv_vm_startup.sh
@@ -1,22 +1,24 @@
 #! /bin/bash
-sh -su
-apt-get -y update
-apt-get -y install default-jdk
-apt-get -y install maven
-apt-get -y install nfs-common
-apt-get -y install git
+sudo apt-get -y update
+sudo apt-get -y install default-jdk
+sudo apt-get -y install maven
+sudo apt-get -y install nfs-common
+sudo apt-get -y install git
 
-nfs_server = $(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/nfs-server -H "Metadata-Flavor: Google")
-nfs_dir = $(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/nfs-dir -H "Metadata-Flavor: Google")
-port = $(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/port -H "Metadata-Flavor: Google")
-filename = $(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/filename -H "Metadata-Flavor: Google")
-component = $(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/component -H "Metadata-Flavor: Google")
+nfs_server=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/nfs-server -H "Metadata-Flavor: Google")
+nfs_dir=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/nfs-dir -H "Metadata-Flavor: Google")
+port=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/port -H "Metadata-Flavor: Google")
+filename=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/filename -H "Metadata-Flavor: Google")
+component=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/component -H "Metadata-Flavor: Google")
 
-mkdir -p $(nfs_dir)
-mount $(nfs_server) $(nfs_dir)
-chmod go+rw $(nfs_dir)
-cd ~/cloud-computing/$(component)
-mvn clean compile:assembly single
+cd ~/
+rm -rf cloud-computing
+git clone https://github.com/gkiran292/cloud-computing.git
+mkdir -p $nfs_dir
+mount $nfs_server $nfs_dir
+chmod go+rw $nfs_dir
+cd ~/cloud-computing/$component
+mvn clean compile assembly:single
 
-nohup java -jar target/$(component)-1.0-SNAPSHOT-jar-with-dependencies.jar -p "$(port)" -d "file:///$(nfs_dir)/$(filename)" &
+java -jar target/$component-1.0-SNAPSHOT-jar-with-dependencies.jar -p "$port" -d "file:///$nfs_dir/$filename"
 # execute task
